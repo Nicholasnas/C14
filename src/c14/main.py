55365f4d4be7fd758779a0759ace316c386287cd
Nicholas <nicholasnas.nn@gmail.com>
2025-08-18 20:02:38 -0300

    Inciando a resolução da atividade 2

--- src/c14/main.py
+++ src/c14/main.py
@@ -1,27 +1,33 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse
 import uvicorn
 import httpx
-import json
 
-app = FastAPI(debug=True)
+
+app = FastAPI()
+
+
+@app.get('/')
+async def hello_api():
+    content = "<h1>Api para obtenção de dados de pokemons</h1>"
+    return HTMLResponse(content)
+    
 
 @app.get("/{pokemon_name}")
-async def get_pokemon(pokemon_name: str):
+async def get_persons(pokemon_name:str):
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}")
+        
+        response = await client.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}')
         
         if response.status_code != 200:
-            raise HTTPException(status_code=response.status_code, detail="Pokémon não encontrado")
+            raise HTTPException(status_code=response.status_code, detail="Pokemon não encontrado")
         
         try:
-            data = response.json()
-            return JSONResponse(
-                content=json.loads(json.dumps(data, indent=4)),  # força identação
-                media_type="application/json"
-            )
+            return response.json()
         except Exception:
-            raise HTTPException(status_code=500, detail="Resposta da API não é JSON válido")
+            raise HTTPException(status_code=500, detail="Resposta da api errada")
+            
+ 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
